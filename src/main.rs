7a7979805862c7788dfc8b587702f32dd1d6b22d
Spike Grobstein <me@spike.cx>
2020-02-04 06:26:43 +0000

    allow for customizing the statefile
    this is needed for running this in a docker container since we may want
    to persist this thing between runs

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod digitalocean;
 
 extern crate ureq;
 
-static STATEFILE: &str = "current_ip.txt";
+static DEFAULT_STATEFILE: &str = "./current_ip.txt";
 
 fn get_ip(endpoint: &str) -> Result<String, std::io::Error> {
     ureq::get(endpoint)
@@ -17,16 +17,16 @@ fn get_ip(endpoint: &str) -> Result<String, std::io::Error> {
         .into_string()
 }
 
-fn write_ip(ip: &str) -> std::io::Result<()> {
-   let mut f = File::create(STATEFILE)?;
+fn write_ip(statefile: &str, ip: &str) -> std::io::Result<()> {
+   let mut f = File::create(statefile)?;
 
    f.write_all(ip.as_bytes())?;
 
    Ok(())
 }
 
-fn read_ip() -> std::io::Result<String> {
-    let mut f = File::open(STATEFILE)?;
+fn read_ip(statefile: &str) -> std::io::Result<String> {
+    let mut f = File::open(statefile)?;
     let mut ip = String::new();
 
     f.read_to_string(&mut ip)?;
@@ -34,33 +34,37 @@ fn read_ip() -> std::io::Result<String> {
     Ok(ip)
 }
 
-fn ip_needs_update(current_ip: &str) -> std::io::Result<bool> {
-    if ! Path::new(STATEFILE).exists() {
+fn ip_needs_update(statefile: &str, current_ip: &str) -> std::io::Result<bool> {
+    if ! Path::new(statefile).exists() {
         // needs to update because the statefile doesn't even exist
         return Ok(true);
     }
 
     // statefile exists, so let's read it in and compare
-    let old_ip = read_ip()?;
+    let old_ip = read_ip(statefile)?;
 
     Ok(old_ip != current_ip)
 }
 
 fn main() -> Result<(), std::io::Error> {
+    let statefile = env::var("STATEFILE").unwrap_or(DEFAULT_STATEFILE.to_string());
+
     let endpoint = match env::var("IP_ENDPOINT") {
         Ok(endpoint) => endpoint,
         _ => panic!("No IP_ENDPOINT set in environment."),
     };
+
     let ip = get_ip(&endpoint)?;
+
     println!("Hello, world: {}", ip);
 
     // testing out some DO stuff
     let config = digitalocean::Config::new_from_environment();
 
 
-    if ip_needs_update(&ip)? {
+    if ip_needs_update(&statefile, &ip)? {
         eprintln!("Needs update. Writing IP to file...");
-        write_ip(&ip)?;
+        write_ip(&statefile, &ip)?;
 
         let record_id = 
             match digitalocean::find_record_id(&config) {
